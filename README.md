972438ef800f4700dd5a08ffd6ec49d76dc070c6
Markus Straub <markus.straub.at@gmail.com>
2017-02-04 16:49:08 +0100

    cleanups, extending features wishlist

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # radlkarte
 
-# Desired Feature List
+## Features
 
-- Show Best routes through city with
-  - four segment quality types: premium, calm, medium, stressful
-  - oneways
-  - Schiebestellen
+- Show recommended bicycle routes
+  - three quality types of route segments: calm, medium, stressful
+  - oneway segments (arrows)
+
+
+### Desired Feature List
+- hanlde oneway arrows properly (width does not change now on zoom)
+- hide parts of the recommended routes when zooming out (two / three importance levels?)
+- add OpenCycleMap? http://thunderforest.com/maps/opencyclemap/
+- add basemap.at as background layer?
+- Visualize short parts where bicycle has to be pushed
 - Geocoding (Google Maps? wien.gv.at?)
 - GPS localization (especially for smartphones)
--- radlkarte.js
+++ radlkarte.js
@@ -1,13 +1,18 @@
 var rkGlobal = {}; // global variable for radlkarte properties / data storage
-rkGlobal.map = undefined; // leaflet-map-object
+
 rkGlobal.debug = false; // debug output will be logged if set to true
+
+rkGlobal.map = undefined; // leaflet-map-object
+rkGlobal.layerControl = undefined; // leaflet layer-control
+
+rkGlobal.markerLines = new Array();
+
 rkGlobal.jsonLayers = new Array();
 rkGlobal.jsonLayersVisible = new Array();
 rkGlobal.layer; // radlkarte-overlay layer displaying the geojson objects
-rkGlobal.showLayer = true;
 rkGlobal.opacity = 0.7;
 rkGlobal.widthFactor = {
-    "premium" : 2,
+    "premium" : 1.2,
     "calm" : 1.2,
     "medium": 0.5,
     "stressful": 0.4
@@ -32,29 +37,25 @@ function loadGeoJson() {
                 if(feature.properties.oneway == 'yes') {
                     // warning latLon expected!
                     //console.log("layer.getLatLngs: " + layer.getLatLngs());
-                    var arrowWidth = getBaseLineWeight() * rkGlobal.widthFactor[feature.properties.ambience] + 8;
-                    L.polylineDecorator(layer.getLatLngs(), {
+                    var arrowWidth = getBaseLineWeight() * rkGlobal.widthFactor[feature.properties.ambience];
+                    arrowWidth = Math.max(arrowWidth * 2, arrowWidth + 8);
+                    var markerLine = L.polylineDecorator(layer.getLatLngs(), {
                         patterns: [
                             {
                                 offset: 25,
                                 repeat: 50,
-                                symbol: L.Symbol.arrowHead({pixelSize: arrowWidth, headAngle: 90, pathOptions: {color: '#FF6600', fillOpacity: rkGlobal.opacity, weight: 0}})
-//                                 symbol: L.Symbol.marker({rotate: true, markerOptions: {
-//                                     icon: L.icon({
-//                                         iconUrl: 'icon.png',
-//                                         iconAnchor: [8, 8]
-//                                     })
-//                                 }})
+                                symbol: L.Symbol.arrowHead({
+                                    pixelSize: arrowWidth,
+                                    headAngle: 90,
+                                    pathOptions: {
+                                        color: '#FF6600',
+                                        fillOpacity: rkGlobal.opacity,
+                                        weight: 0}
+                                })
                             }
                         ]
                     }).addTo(rkGlobal.map);
-
-                    // textpath plugin (not compatible with leaflet 1.x)
-//                     layer.setText('🢂  ', {repeat: true,
-//                             offset: 7,
-//                             attributes: {fill: '#007DEF',
-//                                          'font-weight': 'bold',
-//                                          'font-size': '18'}});
+                    rkGlobal.markerLines.push(markerLine)
                 }
             },
             style: function(feature) {
@@ -68,9 +69,7 @@ function loadGeoJson() {
                 }
             }
         }).addTo(rkGlobal.map);
-        
 
-        
         
         debug('styled geojson. ' + cnt + ' total, ' + cntGood + ' styled');
         
@@ -95,8 +94,9 @@ function loadGeoJson() {
     });
     
     rkGlobal.map.on('zoomend', function(ev) {
-        console.log(rkGlobal.map.getZoom());
+        debug("current zoom level: " + rkGlobal.map.getZoom());
         rkGlobal.layer.setStyle(styleGeoJson);
+        // TODO adapt zoom of arrows
         /*
         var currentZoom = (rkGlobal.map.getZoom()-10)*2.4;
         rkGlobal.layer.eachLayer(function (layer) {
@@ -131,40 +131,41 @@ function getBaseLineWeight() {
 
 
 function initMap() {
-    var normalOpacity = 0.7;
-    var highlightOpacity = 1.0;
-    var normalWeight = 6;
-    var highlightWeight = 8;
-
     rkGlobal.map = L.map('map', { 'zoomControl' : false } ).setView([48.22764,16.40774545], 14);
 
-    var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-    var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-    var emptyAttrib = '&copy; <a href="about.html">Radlkarte contributors</a>';
-    var emptyLayer = L.tileLayer('', {attribution: emptyAttrib});
     var mapboxStreets = L.tileLayer('https://api.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token={accessToken}', {
+        maxZoom: 18,
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="http://mapbox.com">Mapbox</a>',
-        maxZoom: 18,
         accessToken: 'pk.eyJ1IjoiZHRzLWFpdCIsImEiOiJjaW1kbmV5NjIwMDI1dzdtMzBweW14cmZjIn0.VraboGeyXnUjm1e7xWDWbA'
     });
-    var osm = L.tileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});
+    var osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        minZoom: 8,
+        maxZoom: 18,
+        attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+    });
+//     var ocm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//         minZoom: 8,
+//         maxZoom: 18,
+//         attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+//     });
     var mapboxSatellite = L.tileLayer('https://api.tiles.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Imagery &copy; <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18,
+        attribution: 'Imagery &copy; <a href="http://mapbox.com">Mapbox</a>',
         accessToken: 'pk.eyJ1IjoiZHRzLWFpdCIsImEiOiJjaW1kbmV5NjIwMDI1dzdtMzBweW14cmZjIn0.VraboGeyXnUjm1e7xWDWbA'
     });
+    var empty = L.tileLayer('', {attribution: ''});
+    
     var baseMaps = {
-        "None": emptyLayer,
         "OpenStreetMap (Mapbox)": mapboxStreets,
+        "Satellitenbild (Mapbox)": mapboxSatellite,
+        //"OpenCycleMap": ocm,
         "OpenStreetMap": osm,
-        "Satellite (Mapbox)": mapboxSatellite
+        "Leer": empty,
     };
     var overlayMaps = {};
     
-    mapboxStreets.addTo(rkGlobal.map)
-    L.control.layers(baseMaps, overlayMaps, { 'position' : 'topleft', 'collapsed' : false } ).addTo(rkGlobal.map);
-    L.control.zoom().addTo(rkGlobal.map);
-    
+    empty.addTo(rkGlobal.map)
+    rkGlobal.layerControl = L.control.layers(baseMaps, overlayMaps, { 'position' : 'topleft', 'collapsed' : false } ).addTo(rkGlobal.map);
     
     // load overlay & control
     loadGeoJson();
