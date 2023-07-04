f8b59f10f5085b065e8f975de48447f3ccb596dc
Doug Culnane <douglas.culnane@dedalus.com>
2023-07-04 20:14:07 +0200

    fixes

--- radlkarte.js
+++ radlkarte.js
@@ -55,7 +55,7 @@ rkGlobal.configurations = {
 		geoJsonFile: 'data/radlkarte-unteres-rheintal.geojson'
 	},
        'schwarzatal' : {
-                centerLatLng: L.latLng(47.67, 15.97),
+                centerLatLng: L.latLng(47.67, 15.99),
                 geocodingBounds: '15.76,47.67,47.122,16.23,47.81',
                 geoJsonFile: 'data/radlkarte-schwarzatal.geojson'
         },
