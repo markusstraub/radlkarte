050ba2749f128f892d4bbb9f184e14a9e2672484
yyxcv <yxcv@github.com>
2024-09-27 20:43:41 +0200

    wip: init function

--- src/js/module/init.js
+++ src/js/module/init.js
@@ -1,13 +1,13 @@
 import maplibregl from "maplibre-gl";
 import 'maplibre-gl/dist/maplibre-gl.css';
-import testStyle from "./styles/teststyle";
+import baseMapVector from "./styles/basemap_vector";
 
 
 function init() {
 
   const map = new maplibregl.Map({
     container: 'map',
-    style: testStyle,
+    style: baseMapVector,
     center: [ 16.37,48.21],
     zoom: 12,
     hash: true
